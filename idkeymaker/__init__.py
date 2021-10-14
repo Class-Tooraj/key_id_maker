4fed6923d41529527563123233bb80606d82d98a
toorajjahangiri <toorajjahangiri@gmail.com>
2021-10-14 13:53:18 +0330

    create init package file __init__.py

---
+++ idkeymaker/__init__.py
@@ -0,0 +1,12 @@
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> #
+#           < IN THE NAME OF GOD >           #
+# ------------------------------------------ #
+__AUTHOR__ = "ToorajJahangiri"
+__EMAIL__ = "Toorajjahangiri@gmail.com"
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+
+# IMPORT MODULE
+from .base import get_time, rand_char, make_id
+
+
+__all__ = ('get_time', 'rand_char', 'make_id')
